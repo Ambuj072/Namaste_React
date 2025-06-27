a9f5e81e1f89d61aa3505ab1c142d174389e1697
Ambuj072 <ambujdubey072@gmail.com>
2025-06-28 00:39:26 +0530

    with jsx

--- 01React/script.js
+++ 01React/script.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "myid" }, "Hello React");
+const heading = React.createElement("h1", { id: "myid" }, "Hello React🚀");
+
+//using jsx it will convert into React.createElement which is javascript object then render will convert it into html
+
+const jsxHeading=<h1 id="myid">Hello React🚀</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
